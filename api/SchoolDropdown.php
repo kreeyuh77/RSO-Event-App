b8c5128b5707cf9753b4c463899155f5bc9cbd98
Julian Boaz <julian.boaz@gmail.com>
2022-04-05 17:12:44 -0400

    Update SchoolDropdown.php

--- api/SchoolDropdown.php
+++ api/SchoolDropdown.php
@@ -6,12 +6,16 @@
     $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
     if ($conn->connect_error){
         returnWithError($conn->connect_error);
-    } else {
-        // $stmt = $conn->prepare("SELECT Name FROM University");
-        $stmt = $conn->prepare("SELECT json_object(\"Name\", Name) FROM University");
+    } else {        
+        //$stmt = $conn->prepare("SELECT json_object(\"Name\", Name) FROM University");
+        $stmt = $conn->prepare("SELECT Name FROM University");
         $stmt->execute();
         $result = $stmt->get_result();
-        sendResultInfoAsJson($result);
+        $myArray = array();
+        while($row = $result->fetch_array(MYSQLI_ASSOC)){
+            $myArray[] = $row;
+        }
+        sendResultInfoAsJson(json_encode($myArray));
 
         $stmt->close();
         $conn->close();
