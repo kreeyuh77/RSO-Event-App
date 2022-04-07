c6a49a97c9c1ab18787ce584601b935f04ef9216
Julian Boaz <julian.boaz@gmail.com>
2022-04-07 00:52:44 -0400

    Update CreateRSO.php

--- api/CreateRSO.php
+++ api/CreateRSO.php
@@ -19,12 +19,20 @@
         $row = $result->fetch_assoc();
         $stmt->close();
 
-        if ("-1" == $row["RSOAdminID"]){
-            returnWithError("RSO Already Exists");
+        if ("-1" != $row["RSOAdminID"]){
+            returnWithError("Already Admin");
         }
         else {
             $stmt = $conn->prepare("INSERT INTO RSO (Name) VALUES (?)");
             $stmt->bind_param("s", $name);
+            $stmt->execute();
+            $stmt->close();
+            $stmt = $conn->prepare("UPDATE Student SET RSOAdminID = (SELECT id FROM RSO WHERE Name = ?) WHERE StudentID = ?");
+            $stmt->bind_param("ss", $name, $id);
+            $stmt->execute();
+            $stmt->close();
+            $stmt = $conn->prepare("UPDATE Student SET RSOID = (SELECT id FROM RSO WHERE Name = ?) WHERE StudentID = ?");
+            $stmt->bind_param("ss", $name, $id);
             $stmt->execute();
             $stmt->close();
             $conn->close();
