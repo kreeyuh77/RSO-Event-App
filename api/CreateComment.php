9afd27bd6d12a080a96564db0ba759f73be91f0e
Julian Boaz <julian.boaz@gmail.com>
2022-04-11 20:16:22 -0400

    Create CreateComment.php

---
+++ api/CreateComment.php
@@ -0,0 +1,29 @@
+<?php
+    require_once 'DBH.php';
+    require_once 'functions.php';
+
+    $inData = getRequestInfo();
+
+    $comment = $inData["comment"];
+    $event = $inData["eventId"];    
+    $id = $inData["ID"];
+    
+    
+    $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+    if ($conn->connect_error){
+        returnWithError($conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("SELECT Name FROM Student WHERE StudentID = ?");
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $stmt->close();
+        $name = $row["Name"];
+
+        $stmt = $conn->prepare("INSERT INTO Comments (Comment, EventID, StudentName) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $comment, $event, $name);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+    }
