37ff98e79838a68f02c8ec03572acd687970f3cb
Julian Boaz <julian.boaz@gmail.com>
2022-04-11 21:48:43 -0400

    Create CreateRate.php

---
+++ api/CreateRate.php
@@ -0,0 +1,21 @@
+<?
+    require_once 'DBH.php';
+    require_once 'functions.php';
+
+    $inData = getRequestInfo();
+
+    $rate = $inData["rate"];
+    $eventID = $inData["eventId"];
+    $userID = $inData["ID"];
+
+    $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+    if ($conn->connect_error){
+        returnWithError($conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO Ratings (EventID, Rating) VALUES (?, ?)");
+        $stmt->bind_param("ss", $eventID, $rate);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        returnWithError("");
+    }
